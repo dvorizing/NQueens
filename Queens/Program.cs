25a553c1711d45b175350d4daace456017ed8bc6
dvorizing <dvori177359@gmail.com>
2023-10-30 10:57:49 +0200

    first commit

---
+++ Queens/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Queens
+{
+    class Program
+    {
+        
+      
+        static void Main(string[] args)
+        {
+
+            NQueens q = new NQueens(4);
+            q.AllOptions();
+            Console.ReadKey();
+        }
+    }
+}
